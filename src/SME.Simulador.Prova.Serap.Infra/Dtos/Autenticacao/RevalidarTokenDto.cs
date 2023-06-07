8713a60cb6d2cbada685d1e1c43262499237a640
Fernando Groeler <fernando.groeler@gmail.com>
2023-06-07 14:11:36 -0400

    Ajustada instancia da propriedade

--- src/SME.Simulador.Prova.Serap.Infra/Dtos/Autenticacao/RevalidarTokenDto.cs
+++ src/SME.Simulador.Prova.Serap.Infra/Dtos/Autenticacao/RevalidarTokenDto.cs
@@ -5,5 +5,5 @@ namespace SME.Simulador.Prova.Serap.Infra;
 public class RevalidarTokenDto : DtoBase
 {
     [Required(ErrorMessage = "É necessário informar o token.")]
-    public string Token { get; set; }
+    public string Token { get; set; } = string.Empty;
 }
