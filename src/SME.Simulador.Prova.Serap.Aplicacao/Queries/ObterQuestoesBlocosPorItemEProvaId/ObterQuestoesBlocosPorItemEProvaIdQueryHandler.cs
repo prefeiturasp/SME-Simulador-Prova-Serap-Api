be20699ed6cc527c4acc8cf05e7db4497a41b577
fernando.groeler <fernando.groeler@gmail.com>
2023-10-11 15:24:05 -0400

    Ajustado para atualizar todos os cadernos que possuem a questão editada.

---
+++ src/SME.Simulador.Prova.Serap.Aplicacao/Queries/ObterQuestoesBlocosPorItemEProvaId/ObterQuestoesBlocosPorItemEProvaIdQueryHandler.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using SME.Simulador.Prova.Serap.Dados;
+using SME.Simulador.Prova.Serap.Infra;
+
+namespace SME.Simulador.Prova.Serap.Aplicacao;
+
+public class ObterQuestoesBlocosPorItemEProvaIdQueryHandler : IRequestHandler<ObterQuestoesBlocosPorItemEProvaIdQuery, IEnumerable<BlocoItemDto>>
+{
+    private readonly IRepositorioBlocoQuestao repositorioBlocos;
+    
+    public ObterQuestoesBlocosPorItemEProvaIdQueryHandler(IRepositorioBlocoQuestao repositorioBlocos)
+    {
+        this.repositorioBlocos = repositorioBlocos ?? throw new ArgumentNullException(nameof(repositorioBlocos));
+    }
+    
+    public async Task<IEnumerable<BlocoItemDto>> Handle(ObterQuestoesBlocosPorItemEProvaIdQuery request, CancellationToken cancellationToken)
+    {
+        return await repositorioBlocos.ObterQuestoesBlocosPorItemEProvaId(request.ProvaId, request.ItemId);
+    }
+}
